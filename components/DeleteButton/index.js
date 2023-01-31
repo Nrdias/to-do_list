54ee4951e9b69b5fdc75c3344a272a54c9ec3d37
Nrdias <natan.nrdias@hotmail.com>
2023-01-31 17:46:14 -0300

    adding localstorage to save items

--- components/DeleteButton/index.js
+++ components/DeleteButton/index.js
@@ -1,16 +1,24 @@
-export const DeleteButton = () =>{
-    const button = document.createElement('button')
-    button.classList.add('task__button')
-    button.innerText = 'Delete'
-    button.addEventListener('click', deleteTask)
+export const DeleteButton = () => {
+  const button = document.createElement("button");
+  button.classList.add("task__button");
+  button.innerText = "Delete";
+  button.addEventListener("click", deleteTask);
 
-    return button
-}
+  return button;
+};
 
 const deleteTask = (event) => {
-    const deleteButton = event.target
-    const completeTask = deleteButton.parentElement
-    completeTask.remove()
-
-    return deleteButton
-}
+  const deleteButton = event.target;
+  const completeTask = deleteButton.parentElement;
+  completeTask.remove();
+  //remover o item da local storage com base no value
+  const content = completeTask.querySelector(".content").innerText;
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    const value = localStorage.getItem(key);
+    if (value == content) {
+      localStorage.removeItem(key);
+    }
+  }
+  return deleteButton;
+};
--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 import { DoneButton } from "./components/DoneButton";
 import { DeleteButton } from "./components/DeleteButton";
 
-// Estrutura Base
-const app = document.getElementById('app');
-const container = 
-`
+let id = localStorage.length + 1;
+
+const app = document.getElementById("app");
+const container = `
 <div class="form">
   <h1>To-do List</h1>
   <form class= "form__insert" action="">
@@ -17,34 +17,63 @@ const container =
 </div>
 `;
 
-app.innerHTML = container
+app.innerHTML = container;
 
-// Final
+/**
+ * When the form button is clicked, prevent the default action, get the list, get the input, get the
+ * value, if the value is empty or a space, alert the user, otherwise, set the local storage, create a
+ * new task, add the done and delete buttons, append the new task to the list, clear the input, and
+ * increment the id.
+ * @param event - The event object is a property of the Window object.
+ * @returns the button element.
+ */
+const insertTask = (event) => {
+  event.preventDefault();
 
-const insertTask = (event) =>{
-
-  event.preventDefault()
-
-  const list = document.getElementById('list__items');
-  const input = document.getElementById('form__input');
+  const list = document.getElementById("list__items");
+  const input = document.getElementById("form__input");
   const value = input.value;
 
-  if(value == '' || value == ' '){
+  if (value == "" || value == " ") {
     alert("You can't create a empty task");
-    return
-  };
-
+    return;
+  }
+  localStorage.setItem(id, value);
   const content = `<p class="content">${value}</p>`;
-  const newTask = document.createElement('li');
-  newTask.classList.add('item');
+  const newTask = document.createElement("li");
+  newTask.classList.add("item");
   newTask.innerHTML = content;
 
   newTask.appendChild(DoneButton());
   newTask.appendChild(DeleteButton());
   list.appendChild(newTask);
   input.value = " ";
-  
+  input.placeholder = "Describe the task here...";
+  id++;
 };
 
 const formButton = document.getElementById("form__button");
-formButton.addEventListener('click', insertTask);
+formButton.addEventListener("click", insertTask);
+
+/**
+ * For each item in localStorage, create a new list item, add the content of the item to the list item,
+ * add the Done and Delete buttons to the list item, and append the list item to the list.
+ */
+const loadTasks = () => {
+  for (let i = 0; i < localStorage.length; i++) {
+    const list = document.getElementById("list__items");
+    const key = localStorage.key(i);
+    const value = localStorage.getItem(key);
+
+    const content = `<p class="content">${value}</p>`;
+    const newTask = document.createElement("li");
+    newTask.classList.add("item");
+    newTask.innerHTML = content;
+
+    newTask.appendChild(DoneButton());
+    newTask.appendChild(DeleteButton());
+    list.appendChild(newTask);
+  }
+};
+
+loadTasks();
